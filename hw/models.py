29177dbe16155513a8622f0e33a935e40b24aa39
TravLincoln <Travis.dlincoln@gmail.com>
2021-11-08 13:29:46 -0800

    final

--- hw/models.py
+++ hw/models.py
@@ -3,12 +3,12 @@ from hw import db
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    cname = db.Column(db.String(64), unique=True, index=True)
-    rank = db.Column(db.Integer, unique=True)
+    cname = db.Column(db.String(64), unique=False, index=True)
+    rank = db.Column(db.Integer, unique=False)
     posts = db.relationship('Post', backref='author', lazy = 'dynamic')
 
     def __repr__(self):
-        return f'<User {self.id}:  City: {self.cname}, Rank:{self.rank}>'
+        return f'{self.cname}, Rank:{self.rank}'
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
--- hw/routes.py
+++ hw/routes.py
@@ -18,29 +18,30 @@ from hw.forms import TopCities
 @hw_obj.route("/", methods=['GET','POST'])
 def hello():
     name = 'Travis'
-    people = {'Travis' : 25}
     title = 'My HomePage'
-    topcities = ["Paris","London","Los Angeles","Tokyo"]
-    posts = [{'author': 'john', 'body': 'Beautiful day in Portland!'},{'author': 'Susan', 'body': 'Today was a good day!'}]
+    #topcities = ["Paris","London","Los Angeles","Tokyo"]
     form = TopCities()
+    top_cities= User.query.all()
     if form.validate_on_submit():
         user = User.query.filter_by(cname=form.cname.data).first()
-        flash(f'City Chosen:  {form.cname.data}, rank={form.rank.data}')
+        u = User(cname=form.cname.data,rank=form.rank.data)
+        db.session.add(u)
+        db.session.commit()
         return redirect('/')
-    return render_template("home.html",form=form, name=name, people=people, title=title,cities=topcities, posts=posts)
+    return render_template("home.html",form=form,top_cities=top_cities, name=name, title=title)
 
 #@myapp_obj.route("/login", methods=['GET', 'POST'])
 #def login():
    # form = LoginForm()
-    #if form.validate_on_submit():
-       # user = User.query.filter_by(username=form.username.data).first()
-        #if user is None or not user.check_password(form.password.data):
-           # flash('Login Invalid: Username or Password!')
-            #return redirect('/login')
+   # if form.validate_on_submit():
+        #user = User.query.filter_by(username=form.username.data).first()
+    #    if user is None or not user.check_password(form.password.data):
+     #       flash('Login Invalid: Username or Password!')
+      #      return redirect('/login')
        # login_user(user, remember=form.remember_me.data)
-       # flash(f'Login requested for user {form.username.data}, remember_me={form.remember_me.data}')
-       # flash(f'Login password {form.password.data}')
-       # return redirect('/')
+        #flash(f'Login requested for user {form.username.data}, remember_me={form.remember_me.data}')
+        #flash(f'Login password {form.password.data}')
+        #return redirect('/')
    # return render_template("login.html", form=form)
 
 
